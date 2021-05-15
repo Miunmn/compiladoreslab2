524514db8e9246e2b637d9eb1de8e259ffe3ceb1
alejandrotero22 <alejandrotero@Alejandros-Air.lan>
2021-05-14 20:26:35 -0500

    analizador.cpp improved

--- lab2/ejercicio1/analizador.cpp
+++ lab2/ejercicio1/analizador.cpp
@@ -12,7 +12,14 @@ using id = string;
 using num = string;
 
 vector<string> operadores = {"<",">","==","(",")","{","}",";","="};
-vector<char> numeros = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+vector<char> numeros = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9','-'};
+vector<int> cont_lineas;
+
+int numero_linea = 0;
+
+//En caso detecte un error ya no se terminará de analizar el archivo
+template<typename T>
+void ERROR_CARACTER(T valor_no_reconocido){cout<<"\nError en la linea "<<numero_linea<<", caracter '"<<valor_no_reconocido<<"' no soportado"<<endl; exit(1);}
 
 void dfa_relop(relop token_relop)
 {
@@ -93,95 +100,78 @@ void dfa_id(id token_id)
 		if (!(((int) token_id[i]  >=  65 && (int) token_id[i] <=90) || ((int) token_id[i]>= 97 && (int) token_id[i]<=122)))
 		{
       		if(!((find(begin(numeros), end(numeros), token_id[i])) !=numeros.end())){
-				cout << "Caracter o token no soportado: " << token_id << endl;
+				//cout << "Caracter o token no soportado: " << token_id << endl;
+				ERROR_CARACTER(token_id);
 				return;
       		}
 		}
 	}
 	cout << "<ID," << token_id << ">" << endl;
 }
-/*
+
 void dfa_num(num token_num)
 {
 	int cont_dot = 0;
 	bool flag = false;
-	vector<char> other = {'.', 'E', '+', '-'};	
+	vector<char> other = {'.', 'E', '+', '-'};
+	if (token_num[token_num.size()-1] ==';'){
+		token_num.pop_back();
+		flag = true;
+	}	
 	for(int i = 1; i < token_num.size(); i++)
 	{
-		cout<<"NUMM"<<endl;
-		if(!((find(begin(numeros), end(numeros), token_num[i])) != (numeros.end())))
+		switch (token_num[i])
 		{
-			cout<<"NUMM1"<<endl;
-			return;
-		} else if(!((find(begin(other), end(other), token_num[i])) != ( other.end())))
-		{
-			cout<<"NUMM2"<<endl;
-			return;
-		} else if(token_num[i] == '.' && cont_dot == 0)
-		{
-			cont_dot++;
-			cout<<"NUMM3"<<endl;
-
-		} else if((token_num[i] == '+' || token_num[i] == '-') && flag == false)
-		{
-			cout<<"NUMM4"<<endl;
-			return;
-		} else if(token_num[i] == 'E')
-		{
-			flag = true;
-		}
-		
-		if(cont_dot > 1)
-		{
-			cout<<"NUMM5"<<endl;
+		case '0'...'9':
+			break;
+		case '.':
+			break;
+		case 'E':
+			break;
+		case '+':
+			break;
+		case '-':
+			break;
+		default:
 			return;
 		}
 	}
 	cout << "<NUM," << token_num << ">" << endl;
+	if(flag)
+	{
+		cout<<"<ENDOFLINE,';'>"<<endl;
+	}
 }
-*/
+
 void analisis(vector<string> &vector_)
 {
+	int cont_elementos = 0;
+	int i = 0;
     for (string item : vector_)
     {
+		++cont_elementos;
 		if (((int) item[0]  >=  65 && (int) item[0] <=90 ) || ((int) item[0]>= 97 && (int) item[0]<=122 ))
 		{
 			dfa_id(item);
 		} 
-		else if ((find(begin(operadores), end(operadores), string(1,item[0]))) !=operadores.end())
+		else if ((find(begin(operadores), end(operadores), string(1,item[0]))) != operadores.end())
 		{
 			dfa_relop(item);	
 		}
 		else if ((find(begin(numeros), end(numeros), item[0])) != numeros.end())
-		{
-			string aux = item;			
-			istringstream iss(item);
-			istringstream ss(aux);
-			long long int num_int = 0;
-			double num_double = 0;
-			if(!(iss >> num_int).fail())
-			{
-				if (!(ss>> num_double).fail())
-				{
-					if (num_double>num_int)
-					{
-						cout << "<NUM," << num_double << ">" << endl;
-
-					}
-					else
-					{
-						cout << "<NUM," << num_int << ">" << endl;
-					}
-				}
-			}
-
+		{ 
+			dfa_num(item);
 		}
 		else 
 		{
-			cout<<"Caracter o token no soportado: " << item << endl;
+			numero_linea = ++i;
+			ERROR_CARACTER(item);
 			continue;	
-		}	
-		
+		}
+		if(cont_elementos == cont_lineas[i]){
+			i++;
+			cont_elementos = 0;
+		}
     }
 }
 
@@ -197,13 +187,17 @@ void read_data(fstream& file_object,string file_name)
 
 	string input="";
 	vector<string> codigo;
-
+	int cont = 0;
 	while(getline(file_object, input))
 	{
         stringstream ss(input);
 		string word;
-		while(ss >> word)
+		while(ss >> word){
 			codigo.push_back(word);
+			cont++;
+		}
+		cont_lineas.push_back(cont);
+		cont = 0;
     }
 	analisis(codigo);
 	file_object.close();

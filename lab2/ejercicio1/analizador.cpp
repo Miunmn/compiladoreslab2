221e7d8bac5cda71db20395a4ca68ba56c49621d
miunmnn <esteban.principe@utec.edu.pe>
2021-05-17 22:01:04 -0500

    Update

--- lab2/ejercicio1/analizador.cpp
+++ lab2/ejercicio1/analizador.cpp
@@ -11,15 +11,25 @@ using relop = string;
 using id = string;
 using num = string;
 
-vector<string> operadores = {"<",">","==","(",")","{","}",";","="};
+//vector<string> operadores1 = {"<",">","==","(",")","{","}",";","="};
+vector<string> operadores = {"<",">","==","<=",">="};
 vector<char> numeros = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9','-'};
 vector<int> cont_lineas;
 
 int numero_linea = 0;
 
+bool is_id( string item){return ((int) item[0]  >=  65 && (int) item[0] <=90 ) || ((int) item[0]>= 97 && (int) item[0]<=122 );}
+bool is_relop(string item){return (find(begin(operadores), end(operadores), string(1,item[0]))) != operadores.end(); }
+bool is_num( string item){return (find(begin(numeros), end(numeros), item[0])) != numeros.end(); }
+//analisis gramatical
+bool is_term(string item){return (is_id(item) || is_num(item));}
+
+
 //En caso detecte un error ya no se terminará de analizar el archivo
 template<typename T>
-void ERROR_CARACTER(T valor_no_reconocido){cout<<"\nError en la linea "<<numero_linea<<", caracter '"<<valor_no_reconocido<<"' no soportado"<<endl; exit(1);}
+void error_lexico(T valor_no_reconocido){cout<<"\nError en la linea "<<numero_linea<<", caracter '"<<valor_no_reconocido<<"' no soportado"<<endl; exit(1);}
+
+void error_gramatical(string valor_error,string valor_error1){cout<<"\nError gramatical: "<<valor_error<<" "<<valor_error1<<endl; exit(1);}
 
 void dfa_relop(relop token_relop)
 {
@@ -65,7 +75,6 @@ void dfa_relop(relop token_relop)
 				break;
 		}
 	}
-	
 	if(temp == token_relop){
 		if(token_relop.size() == 1 && token_relop == "<")
 			cout << "<REL_OP,'<'>" << endl;
@@ -101,7 +110,7 @@ void dfa_id(id token_id)
 		{
       		if(!((find(begin(numeros), end(numeros), token_id[i])) !=numeros.end())){
 				//cout << "Caracter o token no soportado: " << token_id << endl;
-				ERROR_CARACTER(token_id);
+				error_lexico(token_id);
 				return;
       		}
 		}
@@ -143,29 +152,29 @@ void dfa_num(num token_num)
 	}
 }
 
-void analisis(vector<string> &vector_)
+void analisis_lexico(vector<string> &vector_)
 {
 	int cont_elementos = 0;
 	int i = 0;
     for (string item : vector_)
     {
 		++cont_elementos;
-		if (((int) item[0]  >=  65 && (int) item[0] <=90 ) || ((int) item[0]>= 97 && (int) item[0]<=122 ))
+		if (is_id(item))
 		{
 			dfa_id(item);
 		} 
-		else if ((find(begin(operadores), end(operadores), string(1,item[0]))) != operadores.end())
+		else if (is_relop(item))
 		{
 			dfa_relop(item);	
 		}
-		else if ((find(begin(numeros), end(numeros), item[0])) != numeros.end())
+		else if (is_num(item))
 		{ 
 			dfa_num(item);
 		}
 		else 
 		{
 			numero_linea = ++i;
-			ERROR_CARACTER(item);
+			error_lexico(item);
 			continue;	
 		}
 		if(cont_elementos == cont_lineas[i]){
@@ -175,6 +184,60 @@ void analisis(vector<string> &vector_)
     }
 }
 
+// a > 4 >4 
+
+//last_token 
+void gramatica1(vector<string> &vector_, string value,string value1,int &i)
+{
+	cout<<"value: "<<value<<" value1: "<<value1<<" i: "<<i<<endl;
+	if (value1 == value)
+	{
+		i++;
+		gramatica1(vector_,vector_[i],vector_[i+1],i);
+	}
+	else if(is_relop(value1))
+	{
+		if(is_term(value))
+		{
+			i=i+2;
+			gramatica1(vector_,vector_[i],vector_[i+1],i);
+		}
+		else{
+			error_gramatical(value, value1);
+		}
+	}
+	/*else
+	{
+		ERROR_GRAMATICAL1();
+	}*/
+}
+
+void analisis_sintactico(vector<string> &vector_)
+{
+	//vector<vector<string>> gramatica;
+	int i = 0;
+	for( ; i < vector_.size()-1 ; i=i+1)
+	{
+		//gramatica1
+		if((is_id(vector_[i]) || is_num(vector_[i])) && is_relop(vector_[i+1]))
+		{
+			cout<<"vector_[i]: "<<vector_[i]<<endl;
+			cout<<"vector_[i+1]: "<<vector_[i+1]<<endl;
+			gramatica1(vector_,vector_[i],vector_[i],i);
+			cout<<"\n\n";
+		}
+	}
+	/*
+	for (string &item: vector_)
+	{
+		bool return_gramatica1 = gramatica1() 
+
+		if (is_id(item))
+		{
+			
+		}
+	}*/
+}
 
 void read_data(fstream& file_object,string file_name)
 {
@@ -199,7 +262,11 @@ void read_data(fstream& file_object,string file_name)
 		cont_lineas.push_back(cont);
 		cont = 0;
     }
-	analisis(codigo);
+	cout<<"INICIO ANALISIS LEXICO"<<endl;
+	analisis_lexico(codigo);
+	cout<<"\nINICIO ANALISIS SINTACTICO"<<endl;
+	analisis_sintactico(codigo);
+	cout<<'\n';
 	file_object.close();
 }
 int main()
